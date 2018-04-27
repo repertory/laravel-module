3d0338d8e18353776a6e8264449d3645b8fdb226
wangdong <mail@wangdong.io>
2018-04-27 17:41:46 +0800

    移除多余代码

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -10,43 +10,4 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * 操作成功返回
-     *
-     * @param array|string $data
-     * @return \Illuminate\Http\JsonResponse
-     */
-    final protected function success($data = [])
-    {
-        if (is_string($data)) {
-            $data = [
-                'message' => $data,
-            ];
-        }
-        return response()->json(array_merge([
-            'status' => 'success',
-            'message' => '操作成功',
-        ], $data));
-    }
-
-    /**
-     * 操作失败返回
-     *
-     * @param array|string $data
-     * @return \Illuminate\Http\JsonResponse
-     */
-    final protected function error($data = [])
-    {
-        if (is_string($data)) {
-            $data = [
-                'message' => $data,
-            ];
-        }
-        return response()->json(array_merge([
-            'status' => 'error',
-            'message' => '操作失败',
-            'url' => app('request')->url(),
-        ], $data));
-    }
 }
