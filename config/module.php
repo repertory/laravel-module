9e7437b126770ab1073b290dc1e5e78e77e76c56
wangdong <mail@wangdong.io>
2018-05-31 11:41:48 +0800

    Update module.php

--- config/module.php
+++ config/module.php
@@ -21,11 +21,4 @@ return [
         ]
     ],
 
-    'parse' => [
-        'app_id' => env('PARSE_APPID', ''),
-        'rest_key' => env('PARSE_REST_KEY', ''),
-        'master_key' => env('PARSE_MASTER_KEY', ''),
-        'server_url' => env('PARSE_SERVER_URL', ''),
-        'mount_path' => env('PARSE_MOUNT_PATH', ''),
-    ]
 ];
