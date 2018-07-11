59ab268db1363f2c6e18fb8304a4b5fbb8075b6e
wangdong <mail@wangdong.io>
2018-07-11 22:32:36 +0800

    修复lumen中module:publish命令无法使用问题

--- src/Commands/Publish.php
+++ src/Commands/Publish.php
@@ -3,6 +3,11 @@
 namespace LaravelModule\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\MountManager;
+use League\Flysystem\Filesystem as Flysystem;
+use League\Flysystem\Adapter\Local as LocalAdapter;
 
 class Publish extends Command
 {
@@ -12,7 +17,7 @@ class Publish extends Command
      *
      * @var string
      */
-    protected $signature = 'module:publish';
+    protected $signature = 'module:publish {--force : Overwrite any existing files.}';
 
     /**
      * The console command description.
@@ -21,6 +26,102 @@ class Publish extends Command
      */
     protected $description = 'Publish any publishable assets from module packages';
 
+    protected $files;
+
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+        $this->files = $files;
+    }
+
+    /**
+     * Publish the given item from and to the given location.
+     *
+     * @param  string  $from
+     * @param  string  $to
+     * @return void
+     */
+    protected function publishItem($from, $to)
+    {
+        if ($this->files->isFile($from)) {
+            return $this->publishFile($from, $to);
+        } elseif ($this->files->isDirectory($from)) {
+            return $this->publishDirectory($from, $to);
+        }
+        $this->error("Can't locate path: <{$from}>");
+    }
+    /**
+     * Publish the file to the given path.
+     *
+     * @param  string  $from
+     * @param  string  $to
+     * @return void
+     */
+    protected function publishFile($from, $to)
+    {
+        if (!$this->files->exists($to) || $this->option('force')) {
+            $this->createParentDirectory(dirname($to));
+            $this->files->copy($from, $to);
+            $this->status($from, $to, 'File');
+        }
+    }
+    /**
+     * Publish the directory to the given directory.
+     *
+     * @param  string  $from
+     * @param  string  $to
+     * @return void
+     */
+    protected function publishDirectory($from, $to)
+    {
+        $this->moveManagedFiles(new MountManager([
+            'from' => new Flysystem(new LocalAdapter($from)),
+            'to' => new Flysystem(new LocalAdapter($to)),
+        ]));
+        $this->status($from, $to, 'Directory');
+    }
+
+    /**
+     * Move all the files in the given MountManager.
+     *
+     * @param  \League\Flysystem\MountManager  $manager
+     * @return void
+     */
+    protected function moveManagedFiles($manager)
+    {
+        foreach ($manager->listContents('from://', true) as $file) {
+            if ($file['type'] === 'file' && (!$manager->has('to://' . $file['path']) || $this->option('force'))) {
+                $manager->put('to://' . $file['path'], $manager->read('from://' . $file['path']));
+            }
+        }
+    }
+    /**
+     * Create the directory to house the published files if needed.
+     *
+     * @param  string  $directory
+     * @return void
+     */
+    protected function createParentDirectory($directory)
+    {
+        if (!$this->files->isDirectory($directory)) {
+            $this->files->makeDirectory($directory, 0755, true);
+        }
+    }
+    /**
+     * Write a status message to the console.
+     *
+     * @param  string  $from
+     * @param  string  $to
+     * @param  string  $type
+     * @return void
+     */
+    protected function status($from, $to, $type)
+    {
+        $from = str_replace(base_path(), '', realpath($from));
+        $to = str_replace(base_path(), '', realpath($to));
+        $this->line('<info>Copied ' . $type . '</info> <comment>[' . $from . ']</comment> <info>To</info> <comment>[' . $to . ']</comment>');
+    }
+
     /**
      * Execute the console command.
      *
@@ -28,7 +129,11 @@ class Publish extends Command
      */
     public function handle()
     {
-        $this->call('vendor:publish', ['--provider' => 'LaravelModule\\ServiceProvider']);
+        $publishes = ServiceProvider::pathsToPublish('LaravelModule\ServiceProvider', null);
+        foreach ($publishes as $from => $to) {
+            $this->publishItem($from, $to);
+        }
+        $this->info('Publishing complete.');
     }
 
 }
