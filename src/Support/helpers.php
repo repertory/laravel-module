40b83bec821d1e4d01ef1dbb8fadeee2aafd1223
wangdong <mail@wangdong.io>
2018-07-30 11:47:31 +0800

    移除module方法中路由相关字段

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -20,22 +20,13 @@ if (!function_exists('str_replace_first')) {
 if (!function_exists('module_url')) {
     /**
      * 处理模块路由前缀问题
-     * @param string|array $url
+     * @param string $url
      * @param array $param
      * @return string
      * @throws ReflectionException
      */
     function module_url($url = '', $param = [])
     {
-        if (is_array($url)) {
-            $module = module(join('/', array_filter([
-                snake_case(array_get($url, 'group', '')),
-                snake_case(array_get($url, 'module', '')),
-                last(explode('_', snake_case(array_get($url, 'action', '')), 2))
-            ])));
-            $url = array_get($module, 'url', '');
-        }
-
         $queryString = http_build_query($param);
         $url = trim($url, '/');
         if (!config('module.route.prefix') && !config('module.route.default')) {
@@ -74,18 +65,17 @@ if (!function_exists('module')) {
     /**
      * 获取模块信息
      * @param string $name
-     * @param string $method
      * @return mixed|null
      * @throws ReflectionException
      */
-    function module($name = '', $method = '')
+    function module($name = '')
     {
         static $modules = [];  // 优化多次获取模块信息
 
         $default = trim(config('module.route.default'), '/');
         $prefix = array_filter(explode('/', trim(config('module.route.prefix', ''), '/'))); // 支持前缀
         $url = !empty($_SERVER['REQUEST_URI']) ? parse_url($_SERVER['REQUEST_URI']) : [];
-        $path = trim($name ?: array_get($url, 'path', '/'), '/');
+        $path = trim($name ? : array_get($url, 'path', '/'), '/');
 
         // 前缀处理
         if (count($prefix) && !starts_with($path, implode($prefix, '/'))) {
@@ -104,20 +94,18 @@ if (!function_exists('module')) {
         $name = "{$route->get(count($prefix))}/{$route->get(count($prefix) + 1)}";
         // 默认模块，路由加模块前缀
         if ($default == $name) {
-            $url = trim(str_replace_first($name, '', $subfix), '/') ?: '/';
+            $url = trim(str_replace_first($name, '', $subfix), '/') ? : '/';
         } else {
-            $url = $route->get(count($prefix) + 2) ? "{$name}/{$route->get(count($prefix) + 2)}" : $name;
+            $url = $route->get(count($prefix) + 2) ? "{$name}/{$route->slice(count($prefix) + 2)->implode('/')}" : $name;
         }
-        // 请求方式，默认为get
-        $method = $method ?: (!empty($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'get');
-        $method = strtolower($method);
+
         // 控制器参数
         $group = studly_case(strtolower($route->get(count($prefix))));
         $module = studly_case(strtolower($route->get(count($prefix) + 1)));
-        $action = $method . studly_case(strtolower($route->get(count($prefix) + 2, 'index')));
+        $subfix = $route->slice(count($prefix) + 2)->values()->toArray();
         // 静态变量中获取
-        if (array_has($modules, "{$group}.{$module}.{$action}")) {
-            return array_get($modules, "{$group}.{$module}.{$action}", null);
+        if (array_has($modules, $url)) {
+            return array_get($modules, $url, null);
         }
         // 控制器类名
         $namespace = implode('\\', ['\\Module', $group, $module]);
@@ -137,20 +125,18 @@ if (!function_exists('module')) {
             return null;
         }
         // 保存模块信息到静态变量
-        array_set($modules, "{$group}.{$module}.{$action}", [
+        array_set($modules, $url, [
             'name' => $name,
             'route' => $url,
-            'url' => module_url($url),
+            'subfix' => $subfix,
             'path' => $path,
-            'method' => $method,
             'controller' => $controller,
             'group' => $group,
             'module' => $module,
-            'action' => $action,
             'composer' => $composer,
             'viewpath' => realpath($path . '/views'),
         ]);
-        return array_get($modules, "{$group}.{$module}.{$action}", null);
+        return array_get($modules, $url, null);
     }
 }
 
